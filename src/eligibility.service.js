4cea50a5ae9b2e54df401f19a9b581cb1624accc
faiza-kihel <faizakihel2@gmail.com>
2023-11-20 19:02:08 +0100

    update eligibility.service.js

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -9,6 +9,23 @@ class EligibilityService {
    */
   isEligible(cart, criteria) {
     // TODO: compute cart eligibility here.
+    const shopper = criteria.shopperId == cart.shopperId ? true : false;
+
+    const totalAti = criteria.totalAti.gt < cart.totalAti ? true : false;
+
+    const productId = Boolean(
+      cart.products.find(
+        (p) => p.productId == criteria["products.productId"]["in"]
+      )
+    );
+
+    const date =
+      new Date(criteria.date.and.gt) < new Date(cart.date) &&
+      new Date(cart.date) < new Date(criteria.date.and.lt)
+        ? true
+        : false;
+
+    if (shopper && totalAti && productId && date) return true;
     return false;
   }
 }
